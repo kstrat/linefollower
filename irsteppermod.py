3c755f99b28ea920a60fb87b3a5766841d5b1c44
Cayden Stratford <quuppie@Caydens-Air.T-mobile.com>
2019-03-10 21:28:25 -0600

    newtry

--- irsteppermod.py
+++ irsteppermod.py
@@ -7,8 +7,8 @@ GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left sensor connection
 while True:
   y=GPIO.input(3)                         #Reading output of right IR sensor
   j=GPIO.input(37)                        #Reading output of left IR sensor
-  if y==1:                                #Right IR sensor detects an object
-    print("you are liekly to be eaten by a grue (R)"),y
+  if j==1:                                #Right IR sensor detects an object
+    print("Left"),j
 control_pinsright = [12,16,18,22]
 
 for pin in control_pinsright:
@@ -31,16 +31,17 @@ for i in range(512):
     for pin in range(4):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
+time.sleep(0.1)
 
-  time.sleep(0.1)
-  if j==1:                              #Left IR sensor detects an object
-    print("you are likely to be eaten by a grue (L)"),j
+if y==1:                              #Left IR sensor detects an object
+    print("Right"),y
+
 control_pinsleft = [7,11,13,15]
 
 for pin in control_pinsleft:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
-halfstep_seq = [
+  halfstep_seq = [
   [1,0,0,1],
   [0,0,0,1],
   [0,0,1,1],
@@ -57,7 +58,7 @@ for i in range(512):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
 
-  time.sleep(0.1)
+time.sleep(0.1)
 
 GPIO.setmode(GPIO.BOARD)
 
